c2ed2afc4acb52f4ec2573bf599b329738d3049f
Olli Jarva <olli.jarva@solinor.fi>
2018-01-29 12:55:16 +0200

    Inherit from ModelBackend
    This is to allow using Django's groups and roles

--- googleauth/backends.py
+++ googleauth/backends.py
@@ -1,13 +1,14 @@
 import requests
 from django.conf import settings
 from django.contrib.auth.models import User, Group
+from django.contrib.auth.backends import ModelBackend
 
 IS_STAFF = getattr(settings, 'GOOGLEAUTH_IS_STAFF', False)
 GROUPS = getattr(settings, 'GOOGLEAUTH_GROUPS', tuple())
 APPS_DOMAIN = getattr(settings, 'GOOGLEAUTH_APPS_DOMAIN', None)
 
 
-class GoogleAuthBackend(object):
+class GoogleAuthBackend(ModelBackend):
 
     def authenticate(self, identifier=None, attributes=None):
 
