9d494dae1817299aece496f1a24c9e31c24003f8
Graham Lyon <graham.lyon@updraft.com>
2021-11-03 18:59:56 +0000

    Switch to re_path()
    I'm sick of these warnings when I run the tests.

--- googleauth/urls.py
+++ googleauth/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.urls import re_path
 import googleauth.views
 
 urlpatterns = [
-    url(r'^login/$', googleauth.views.login, name='googleauth_login'),
-    url(r'^callback/$', googleauth.views.callback, name='googleauth_callback'),
-    url(r'^logout/$', googleauth.views.logout, name='googleauth_logout'),
+    re_path(r'^login/$', googleauth.views.login, name='googleauth_login'),
+    re_path(r'^callback/$', googleauth.views.callback, name='googleauth_callback'),
+    re_path(r'^logout/$', googleauth.views.logout, name='googleauth_logout'),
 ]
